5834cde146fc7f10576f6a85e31c3054e4fd46ce
tomoya <tomoya.kamaji@gmail.com>
2023-07-05 21:34:13 +0900

    validater

--- src/domain/user/user.ts
+++ src/domain/user/user.ts
@@ -4,14 +4,26 @@ export class User {
   readonly age: number | undefined
 
   constructor(name: string, age: number | undefined) {
+    this.id = 1
     this.name = name
     this.age = age
   }
+
   static create(name: string, age: number | undefined) {
+    validate(name, age)
     return new User(name, age)
   }
 
   static reconstruct(name: string, age: number | undefined) {
     return new User(name, age)
+  }
+}
+
+function validate(name: string, age: number | undefined): Error | undefined {
+  if (name.length < 3) {
+    return new Error('Name is too short')
+  }
+  if (age && age < 0) {
+    return new Error('Age is too small')
   }
 }
